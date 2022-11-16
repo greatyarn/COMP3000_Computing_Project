a74a19f8b2f756a959171b92044cdb25abf2ba0f
Gregory Kua <72383536+greatyarn@users.noreply.github.com>
2022-11-16 15:33:56 +0000

    Added a test speechsay to check if it works

--- AAVR/src/main.py
+++ AAVR/src/main.py
@@ -2,13 +2,23 @@
 import sys
 import rospy
 
+from qt_robot_interface.srv import *
+from qt_vosk_app.srv import *
+from qt_gesture_controller.srv import gesture_play
+from qt_nuitrack_app.msg import Gestures
+
+def greet():
+    # greeting
+    speechSay("Hello! This is the startup process")
+    speechSay("This is a test")
+
 if __name__ == '__main__':
     rospy.init_node('main')
-    rospy.loginfo("my_tutorial_node started!")
+    rospy.loginfo("AAVR started")
 
     try:
         rospy.spin()
     except KeyboardInterrupt:
         pass
 
-    rospy.loginfo("finsihed!")
+    rospy.loginfo("finished!")
