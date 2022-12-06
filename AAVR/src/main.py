a47336a528466149bca421020c12dd29e1231d1c
Gregory Kua <72383536+greatyarn@users.noreply.github.com>
2022-12-06 13:12:34 +0000

    Added a delay, To allow for user to start recording

--- AAVR/src/main.py
+++ AAVR/src/main.py
@@ -10,20 +10,31 @@ AUDIO_WIDTH = 2
 def channel_callback(msg, wf):
     wf.writeframes(msg.data)
 
+# Define Speech Service Class
+speechSay = rospy.ServiceProxy('/qt_respeaker_app/speech_say', SpeechSay)
+
 # main
 if __name__ == '__main__':
 
     # call the relevant service
     rospy.init_node('audio_record')
     
-    # Makes a new file called "recording.wav" 
+   
+    
+    # Channel 0 is used because it is the processed audio from the microphone
+    rospy.Subscriber('/qt_respeaker_app/channel0', AudioData, channel_callback, wf)
+
+    # Says "recording" and waits for the user to say something
+    speechSay("recording in 5 seconds")
+
+    # Waits for 5 seconds
+    rospy.sleep(5)
+    
+     # Makes a new file called "recording.wav" 
     wf = wave.open("recording.wav", 'wb')
     wf.setnchannels(AUDIO_CHANNELS)
     wf.setsampwidth(AUDIO_WIDTH)
     wf.setframerate(AUDIO_RATE)
-    
-    # Channel 0 is used because it is the processed audio from the microphone
-    rospy.Subscriber('/qt_respeaker_app/channel0', AudioData, channel_callback, wf)
 
     print("recording...")
     rospy.spin()
