ae93c48f56e490a7072ad70701c26e02abce4c5d
Gregory Kua <72383536+greatyarn@users.noreply.github.com>
2023-03-02 18:05:57 +0000

    Updated main.py. starting email

--- AAVR/src/email_send.py
+++ AAVR/src/email_send.py
@@ -1,2 +1,7 @@
 from main import *
+import smtplib
 
+def email_upload():
+    # Define the email address and password
+    email_address = "
+    
--- AAVR/src/main.py
+++ AAVR/src/main.py
@@ -2,6 +2,33 @@ import email_send as eu
 import rospy
 from qt_robot_interface.srv import *
 
+def userSave():
+    try:
+        speechSay("State your Name")
+    except rospy.ServiceException as e:
+        print("Service call failed: %s" % e)
+
+    #wait for the user to speak
+    rospy.sleep(5)
+
+    user_name = recognise("en-US")
+    rospy.loginfo(user_name)
+
+    speechSay("Hello " + user_name + " Is this the right name?")
+    rospy.sleep(5)
+
+    try:
+        confirmation = recognise("en-US")
+        if confirmation == "yes":
+            rospy.loginfo("Yes")
+            speechSay("Ok, I will remember that")
+        else:
+            rospy.loginfo("No")
+            speechSay("Let's try that again!")
+            userSave()
+    except rospy.ServiceException as e:
+        print("Service call failed: %s" % e)
+        
 if __name__ == '__main__':
 
     # Define ROS Services
@@ -17,16 +44,8 @@ if __name__ == '__main__':
     rospy.wait_for_service('/qt_robot/speech/say')
     rospy.wait_for_service('/qt_robot/speech/recognize')
 
-    # Log that the service is now available
-    try:
-        speechSay("State your Name")
-    except rospy.ServiceException as e:
-        print("Service call failed: %s" % e)
+    userSave()
 
-    #wait for the user to speak
-    rospy.sleep(5)
-
-    
     # Call the email_upload function
     eu.email_upload()
     
