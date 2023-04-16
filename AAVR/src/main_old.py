41b87257bb57bb0d15d4187692ab560880a4b2bc
Gregory Kua <72383536+greatyarn@users.noreply.github.com>
2023-04-16 16:46:15 +0100

    Updating to one file

--- AAVR/src/main_old.py
+++ AAVR/src/main_old.py
@@ -2,10 +2,6 @@
 import rospy
 from qt_robot_interface.srv import *
 import db as db
-import smtplib
-import os
-from dotenv import load_dotenv, find_dotenv
-
 
 def otpCreate():
     import random
@@ -55,41 +51,6 @@ def userSave():
             userSave()
     except rospy.ServiceException as e:
         print("Service call failed: %s" % e)
-
-
-def email_send():
-    load_dotenv(find_dotenv())
-
-    # Email Address for sender is written here
-    email_address = os.getenv("EMAILSEND")
-    password = os.getenv("EMAILPASS")
-
-    # Email Address for receiver is written here
-    email_address_receiver = os.getenv("EMAILSEND")
-
-    # Email Subject
-    subject = "Hello! Here is the OTP that you requested " + user_name
-
-    # Email Body
-    body = "Your OTP is " + otp + \
-        "Please say this OTP to verify your account to the robot once requested. Thank you very much!"
-
-    try:
-        # SMTP
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        print("Connection to mail server established")
-        # server.ehlo()
-        print("a")
-        server.starttls()
-        print("b")
-        server.login(email_address, password)
-        print("c")
-        server.sendmail(email_address, email_address_receiver, subject + body)
-        print("D")
-        server.quit()
-    except Exception as E:
-        print(str(E))
-
 
 if __name__ == '__main__':
 
