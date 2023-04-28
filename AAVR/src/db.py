bcf01853f1087e8984d34d5b8811d9a60dfa1ade
Gregory Kua <72383536+greatyarn@users.noreply.github.com>
2023-04-28 16:19:13 +0100

    Update db.py

--- AAVR/src/db.py
+++ AAVR/src/db.py
@@ -18,6 +18,9 @@ def upload_user(user_name, otp):
     # Create a cursor object
     cursor = conn.cursor()
 
+    user_name = str.user_name
+    otp = int.otp
+
     # upload user data
     cursor.execute(
         "INSERT INTO users (user_name, otp) VALUES (%s, %s)", (user_name, otp))
